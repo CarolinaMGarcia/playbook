dccdcfd4809b38e3919800ce557b30c9e6aec322
Carolina G <ccarolina_mg@hotmail.com>
2022-04-19 00:13:35 -0500

    ejercicio9

---
+++ weekly_mission_1/example9/pokemon.js
@@ -0,0 +1,20 @@
+
+class pokemon{
+
+constructor(name)
+{
+    this.name = name
+}
+sayHello() {
+    console.log(`Mi pokemon ${this.name} te saluda!!!`)
+  }
+
+sayMessage (message) {
+    console.log(`Mi pokemon ${this.name} dice: ${message}`)
+  }
+
+    
+    }
+module.exports = pokemon
+
+
